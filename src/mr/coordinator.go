b39da7abbdae766929bb6169a9267f1ac287da2a
Rex Ledesma <rex@rexledesma.com>
2025-05-21 16:02:24 -0400

    refactor(mapreduce): remove example rpc

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -113,14 +113,6 @@ func (c *Coordinator) CompleteTask(args *CompleteTaskArgs, reply *CompleteTaskRe
 	return nil
 }
 
-// an example RPC handler.
-//
-// the RPC argument and reply types are defined in rpc.go.
-func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
-	return nil
-}
-
 // start a thread that listens for RPCs from worker.go
 func (c *Coordinator) server() {
 	rpc.Register(c)
